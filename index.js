e93be45008915cd69c233e6d03f47b4cbf3ab6ae
Tymchyk Maksym <maksym.tymchyk@gmail.com>
2019-04-15 13:13:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function listFAQ(key) {
+    return [];
+}
+
+function parceFAQItem(content) {
+    return {
+        title: "How to Mine wallet",
+        content: "How to be **cooler**"
+    };
+}
+
+export.default = { listFAQ: listFAQ, parceFAQItem: parceFAQItem };
